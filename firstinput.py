87efc3dc2279ac5cb8f923b2a4fdbf4b455b51f3
lordhefe <101816036+lordhefe@users.noreply.github.com>
2022-03-29 11:46:14 -0400

    Add files via upload

---
+++ firstinput.py
@@ -0,0 +1,50 @@
+# get first and last name
+
+""" get firstname
+then get lastname """
+
+first_name = input("Enter first name please: ")
+
+last_name =input("Enter last name please: ")
+
+# combine both names
+
+name = first_name +" "+ last_name
+
+# say hello
+print("hello" , name)
+----------------------------------------------------------
+# ask user to enter two numbers
+
+
+num1= int(input("Enter first number: "))
+
+
+num2= int(input("Enter first number: "))
+
+
+total = num1 + num2
+
+# display results
+
+print("Result:", total)
+------------------------------------------------------------
+# ask user to enter two numbers
+
+
+num1= float(input("Enter first number: "))
+print()
+
+num2= float(input("Enter first number: "))
+
+
+total = num1 + num2
+
+# display results
+
+print("\nResult:", total)
+
+input("Press any key to exit")
+
+-------------------------------------------------
+end and sep are different endings
