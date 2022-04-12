f020df2ee2b453630e7924ada84d7d26c6f2fef0
lordhefe <101816036+lordhefe@users.noreply.github.com>
2022-04-12 11:01:51 -0400

    Add files via upload

---
+++ 12apr22.py
@@ -0,0 +1,63 @@
+
+
+print("Good Morning Mr. Stupid")
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+
+
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+print ("Good morning Mr Stoopid")
+
+
+for name in ["Mr Stoopid","Mr Stoopid","Mr Stoopid","Mr Stoopid"]:
+
+    print ("Good morning", name)
+
+
+for num in [1,2,3,4,5]:
+        cube = num **3
+    print (num, num **2, cube )
+
+
+    print(f' {num:<5}{num**2<5:}{cube')
+
+print(f'{num":<5}{"Sqr":<5}{cube}')
+print ("----------------")
+for num in [1,2,3,4,5]:
+    
+        cube = num **3
+
+    print(f' {num:<5}{num**2<5:}{cube')
+
+
+
+for num in range(5):
+    # range will go up to but not include the number
+        cube = num **3
+
+    print(f' {num:<5}{num**2<5:}{cube')
+
+start = int(input("Enter Starting Point: "))
+end = int(input("Enter Ending Number: "))
+    for num in range(start, end+1):
+  
+        cube = num **3
+
+    print(f' {num:<5}{num**2<5:}{cube')
+
+
+
+
+start = int(input("Enter Starting Point: "))
+end = int(input("Enter Ending Number: "))
+    for num in range(start, end+1,2):
+# 3rd option is step value  
+        cube = num **3
+
+    print(f' {num:<5}{num**2<5:}{cube')
+
+    
---
+++ 12apr22tip.py
@@ -0,0 +1,25 @@
+# price of food
+
+pre_tip = float(input("Enter Price of Food: "))
+
+# calculate 18% tip (price * .18)
+
+
+tip = float(input("How much to tip? (.15, .20) "))
+
+if tip == .15:
+
+tipPer = pre_tip * 0.15
+
+elif tip == .20:
+    tipPer = pre_tip * 0.20
+
+    else:
+        print ("Invalicd entry!")
+        print("20% tip will be applied")
+
+# calculate total price , add the above
+
+total = pre_tip + tipPer
+
+print("total is $", total, sep="")
