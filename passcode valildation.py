8f1106b1f30bcf5c0b4b9397e1f2e659126051c7
lordhefe <101816036+lordhefe@users.noreply.github.com>
2022-04-14 11:44:16 -0400

    Add files via upload

---
+++ passcode valildation.py
@@ -0,0 +1,35 @@
+passCode = input ("Enter pass code: ")
+
+count = 1
+while (passCode not it ["1234", "65463"]) and count < 3:
+    count = count +1
+    
+    print("Invalid")
+    
+    passCode = input ("Enter pass code again!! ")
+
+    if count > 3:
+
+        print("\nGOODBYE!!")
+        print("")
+    else:
+        
+num_People = int(input("how many people do you eant to evaluate loan status."))
+for x in range(num_People):
+        print("/nCalculating loan number:", x)
+        
+        salary = float( input("enter years on job please:"))
+        
+    if salary >= 30000 and years >= 2:
+
+        print("you qualify")
+        else:
+
+        print("you dont qualify")
+
+        if salary >= 30000 or years >= 2:
+
+        print("you qualify")
+    else:
+
+        print("you dont qualify")
---
+++ score 2.0.py
@@ -0,0 +1,22 @@
+score = float (input("Enter score Please:") )
+
+while score >100 or score <0 :
+    print("\nInvalid Score Entered!!")
+    print("Must be between 0 and 100")
+
+    score = float(input("enter score again: "))
+
+if score >=90:
+    print("A")
+else:
+    if score >=80:
+    print(B")
+else:
+     if score >=70:
+         print("C")
+else:
+     if score >=60:
+         print("D")
+else:
+     if score <60:
+         print("F")
